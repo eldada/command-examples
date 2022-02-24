abbe4f29a2d95a06fc71319bfe7ec70a5b3fedfd
Eldad Assis <eldada@jfrog.com>
2022-02-24 08:58:03 +0200

    Improve performance and output

--- scripts/memory.sh
+++ scripts/memory.sh
@@ -61,6 +61,7 @@ processOptions () {
 
 main () {
     processOptions "$@"
+    local s=
     local str=
 
 #    echo "My PID is $$"
@@ -69,10 +70,18 @@ main () {
 
     # Generating objects
     # Create a 1KB string
-    str="1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstu"
+    echo "> Creating a 1KB string"
+    s='1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ____'
+    for a in $(seq 1 8); do
+        str="${str}${s}"
+#        echo -ne "* $a\r"
+    done
+
+    echo "(String length is ${#str} bytes)"
+
     echo "> Creating ${SIZE_KB} 1KB objects... "
     for i in $(seq 0 "${SIZE_KB}"); do
-        eval array"$i"="${str}"
+        eval array"$i"=${str}
 
         # Save io by reading memory only every 50 iterations
         if ! (( i % 50 )); then echo -ne "Current memory: $(grep VmRSS /proc/$$/status | awk '{print $2}')\r"; fi
